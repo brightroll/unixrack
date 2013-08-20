ad564cbb7f739b768549ca131b99f2f63b2801d7
Dru Nelson <drudru@gmail.com>
2013-08-20 02:46:57 +0000

    adding some headers for operation behind proxies

--- unixrack.rb
+++ unixrack.rb
@@ -441,7 +441,15 @@ module Rack
               if sock.headers['Range']
                 env["HTTP_RANGE"] = sock.headers['Range']
               end
-
+              if sock.headers['X-Real-IP']
+                env["HTTP_X_REAL_IP'"] = sock.headers['Real-IP']
+              end
+              if sock.headers['X-Forwarded-For']
+                env["HTTP_X_FORWARDED_FOR'"] = sock.headers['X-Forwarded-For']
+              end
+              if sock.headers['Host']
+                env["HTTP_HOST"] = sock.headers['Host']
+              end
 
               env["HTTP_VERSION"] = "HTTP/1.1"
               if sock.headers['If-Modified-Since']
