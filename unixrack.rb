1dd842984ca4822af6f74ae91caa04716a88de5f
Aaron Stone <aaron@serendipity.cx>
2013-08-28 13:14:37 -0700

    Ruby 1.8.7 backwards compatibility for set_encoding

--- unixrack.rb
+++ unixrack.rb
@@ -379,7 +379,7 @@ module Rack
 
               if sock.hdr_method[0] == "GET"
                 content = StringIO.new("")
-                content.set_encoding(Encoding::ASCII_8BIT)
+                content.set_encoding(Encoding::ASCII_8BIT) if content.respond_to?(:set_encoding)
               elsif sock.hdr_method[0] == "POST"
                 if not sock.headers.include?('Content-Length')
                   send_error_response!(sock, 400, "Bad Request no content-length", sock.hdr_method[0], sock.hdr_method[1])
@@ -402,7 +402,7 @@ module Rack
 
                 # It is required that we read all of the content prior to responding
                 content = sock.read_content
-                content.set_encoding(Encoding::ASCII_8BIT)
+                content.set_encoding(Encoding::ASCII_8BIT) if content.respond_to?(:set_encoding)
 
                 if content == nil
                   send_error_response!(sock, 400, "Bad Request not enough content", sock.hdr_method[0], sock.hdr_method[1])
