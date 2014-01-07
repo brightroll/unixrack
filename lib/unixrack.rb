3a3a01f1ce692cc7f4c0898fe720c4f607a57e12
Dru Nelson <drudru@gmail.com>
2014-01-07 09:21:31 +0000

    fix issue with unicode strings

--- lib/unixrack.rb
+++ lib/unixrack.rb
@@ -57,10 +57,11 @@ module UnixRack
 
       out_buff = buff
 
+      # buff should be UTF-8
       while true
         nw = io.syswrite(out_buff)
         nwritten = nwritten + nw
-        break if nw == out_buff.length
+        break if nw == out_buff.bytesize
         out_buff = out_buff.slice(nw..-1)
       end
       nwritten
